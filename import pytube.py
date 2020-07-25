4679e5303d4cc9889521aaf5d15fe78299bf4716
remonhob <54642491+remonhob@users.noreply.github.com>
2020-07-25 14:36:20 +0200

    Delete import pytube.py

--- import pytube.py
+++
@@ -1,9 +0,0 @@
-from pytube import YouTube
-import os
- 
-# download a file with only audio, to save space
-# if the final goal is to convert to mp3
-youtube_link = 'https://www.youtube.com/watch?v=YNP9fXZj32g'
-y = YouTube(youtube_link)
-t = y.streams.filter(only_audio=True).all()
-t[0].download(output_path='C:\Temp\Video')
